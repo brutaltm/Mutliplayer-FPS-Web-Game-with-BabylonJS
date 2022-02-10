c85adecf744c54996e03fc338a613e7c6d6ad03e
brutaltm <bartosz.ruta8@o2.pl>
2022-02-10 16:19:51 +0100

    Readme

--- AuthServer/CreateDatabase.js
+++ AuthServer/CreateDatabase.js
@@ -7,32 +7,34 @@ let db = new sqlite3.Database('./authServer.db', (err) => {
 		console.error(err.message);
 	else
 		console.log('Connected to the authServer database.');
-});
 
-var createTableString = 
-	'CREATE TABLE users (' +
-	   'id INTEGER PRIMARY KEY,' +
-	   'email TEXT NOT NULL UNIQUE,' + 
-	   'username TEXT NOT NULL UNIQUE,' +
-	   'password TEXT NOT NULL,' +
-	   'admin INTEGER' +
-	')';
-	
-var dropTableString = 'DROP TABLE users';
-	
-// bcrypt.hash('admin', saltRounds, (err, hash) => {
-	// var insertString = 'INSERT INTO users (email,username,password,admin) VALUES (' + 
-	// '\'bartosz.ruta8@o2.pl\',\'brutaltm\','+'\''+hash+'\',TRUE)';
-	// console.log("Insert",insertString,'\n');
-	// db.run(dropTableString);
-	// db.run(createTableString, (e) => {
-		// db.run(insertString);
 
-		// db.close();
-	// });
-// });
-db.all('SELECT * FROM users',(err, rows ) => {
-    console.log(rows);   
+	var createTableString = 
+		'CREATE TABLE users (' +
+		   'id INTEGER PRIMARY KEY,' +
+		   'email TEXT NOT NULL UNIQUE,' + 
+		   'username TEXT NOT NULL UNIQUE,' +
+		   'password TEXT NOT NULL,' +
+		   'admin INTEGER' +
+		')';
+		
+	var dropTableString = 'DROP TABLE users';
+		
+
+	db.all('SELECT * FROM users',(err, rows ) => {
+		console.log(rows);   
+		bcrypt.hash('admin', saltRounds, (err, hash) => {
+			var insertString = 'INSERT INTO users (email,username,password,admin) VALUES (' + 
+			'\'admin@admin.com\',\'brutaltm\','+'\''+hash+'\',TRUE)';
+			console.log("Insert",insertString,'\n');
+			//db.run(dropTableString);
+			//db.run(createTableString, (e) => {
+				db.run(insertString);
+
+			//	db.close();
+			//});
+		})
+	});
 });
 
 
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Mutliplayer FPS Web Game with BabylonJS
  Made with BabylonJS, WebSocket
+
+ Demo (SinglePlayer) on Heroku: https://fpsgamedemo.herokuapp.com/ (Site might take couple seconds to launch)
+ 
+ ClientApp - Actual game made with Babylon.js
+ GameServer - Game server for multiplayer
+ AuthServer - Authorization Server
+ 
+ ### Built With
+
+* [Babylon.js](https://babylonjs.com/)
+* [uWebSockets](https://github.com/uNetworking/uWebSockets.js/)
+
+ 
+ <!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE` for more information.
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+
+* [Ragdoll.js](https://github.com/jongomez/ragdoll.js/)
+* [Oimo.js](https://github.com/lo-th/Oimo.js/)
+* [Low Poly AK-47](https://skfb.ly/opD99) by lokeig
+* [Ak47 Sound] (https://freesound.org/s/509430/) by seanmorrissey96
